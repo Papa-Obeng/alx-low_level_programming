8e69d2884d10f10d9d430982f3f9ca1be4144b24
Papa-Obeng <papaobengn@gmail.com>
2022-12-11 14:31:28 -0800

    Update on Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
+\**
+*The function is to print the string of words
+*It starts with entry from the main function
+*The pusts function prints the string
+* it ends by printing a value of zero
+* \
 
 int main(){puts("\"Programming is like building a multilingual puzzle"); return (0); }

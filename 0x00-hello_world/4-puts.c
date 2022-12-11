99a9f337ae97ef0783ec701e6a743807a730cd14
Papa-Obeng <papaobengn@gmail.com>
2022-12-11 15:10:54 -0800

    Update,Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-\**
-*The function is to print the string of words
-*It starts with entry from the main function
-*The pusts function prints the string
-* it ends by printing a value of zero
-* \
-
-int main(){puts("\"Programming is like building a multilingual puzzle"); return (0); }
+/** The main function print the strings of words
+ * Using the escape character, the puts function prints the strings
+ * the return function evaluates to zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+       	return (0);
+}

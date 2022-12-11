8d00e76f17af25e303e276676672c46fe127fcbe
Papa-Obeng <papaobengn@gmail.com>
2022-12-11 15:35:48 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main-entry to the program that prints the strings using printf
+ * return function evaluates to zero
+ */
+#include<stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}

ea418f7d11c5e48f56b812a35fcb139d4e64ae7b
Papa-Obeng <papaobengn@gmail.com>
2022-12-12 03:54:31 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point  Prints strings
+ * Return: Always 1 (Success)
+ */
+
+int main()
+{
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-1			0-19\n",
+		59);
+	return (1);
+}

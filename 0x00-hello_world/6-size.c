91be5aabefe0f267c1616f3cad5c6c4a7f3630e9
Papa-Obeng <papaobengn@gmail.com>
2022-12-12 03:13:35 -0800

    Updated Datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
-/** main- entry to the program
+/**
+ * main - Entry point
  * the sizeof function helps us to get the size of the datatypes
  * the printf function prints the sizes
- * the return function evaluates to zero
+ * Return: 0 (Success)
  */
 #include <stdio.h>
 
@@ -12,6 +13,6 @@ int main(void)
 	printf("Size of a long int %zu\n: " sizeof(long int) "bytes");
 	printf("Size of a long long int %zu\n: " sizeof(long long int) "bytes");
 	printf("Size of a float %zu\n: " sizeof(float) "bytes");
-	return(0);
+	return (0);
 }
 

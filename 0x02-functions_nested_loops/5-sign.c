06147e8719c2497acce550e5db07df54d63acca6
Papa-Obeng <papaobengn@gmail.com>
2022-12-14 13:45:30 -0800

    Print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
-#include "main.h"                                                                                                                     
+#include "main.h"                                                                                                          
 
-/**                                                                                                                                   
- * print_sign - a function that prints the sign of a number                                                                  
+/**                                                                                                                     
+ * print_sign - a function that prints the sign of a number
  *
- * @n: number input 
+ * @n: number input
  * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n<0
  */
 
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (0);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
+		_putchar(45);
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
+		_putchar(48);
 		return (0);
 	}
 }

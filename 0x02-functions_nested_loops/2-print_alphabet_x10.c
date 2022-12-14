6601f9bd85df52a1e608d0b07453dbd41531f3d7
Papa-Obeng <papaobengn@gmail.com>
2022-12-14 10:50:16 -0800

    Tentimes_lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,16 +9,19 @@
 void print_alphabet_x10(void)
 
 {
-	int n, co;
+	int co;
 
 	co = 0;
 
 		while (co < 10)
 		{
+			int n;
 
 			for (n = 'a'; n <= 'z'; n++)
+			{
 			_putchar(n);
 			_putchar('\n');
-		}
+			}
 		co++;
+		}
 }

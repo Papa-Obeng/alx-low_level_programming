bbbcf6dd294cb602ed95539bf377e4ccc4f9cdf9
Papa-Obeng <papaobengn@gmail.com>
2022-12-16 10:28:47 -0800

    Update Isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,20 +4,12 @@
  * -isdigit funtion - The function that checks whether the input is a digit
  *  @c: The input number to be checked
  *
- *  Returned: Always 0 (Success)
+ *  Return: Returns boolean
  */
 
 int _isdigit(int c)
 {
-	int a = 48, b = 57;
-
-	if (c >= a || c <= b)
-	{
+	if (c >= '0' && c <= '9')
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
 	return (0);
 }

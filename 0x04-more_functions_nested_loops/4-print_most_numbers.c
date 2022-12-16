27e8a8c5aa826837e3fe8f765176b0e5285f32f4
Papa-Obeng <papaobengn@gmail.com>
2022-12-16 07:44:59 -0800

    Print without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 t 9 except 2 and 4
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a == '2' || a == '4')
+		{
+			_putchar(',');
+		}
+		else
+			_putchar(a);
+	}
+}

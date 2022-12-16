45d3e57a0675397b529573f76e1735a831234319
Papa-Obeng <papaobengn@gmail.com>
2022-12-16 10:37:14 -0800

    Updated mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,12 +4,10 @@
  * mul function - The mul function will multiply two integers
  * @a, @b - The two integer input to be multiplied
  * result - the variable to hold the value after multiplication
- * Return: result (Success)
+ * Return: result
  */
 
 int mul(int a, int b)
 {
-	int result = a * b;
-
-	return (result);
+	return (a * b);
 }

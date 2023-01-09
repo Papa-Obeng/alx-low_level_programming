db1004d6eb499face6b57ce6bc6a8b1bee62f83f
Papa-Obeng <papaobengn@gmail.com>
2023-01-09 15:14:50 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - function to print the number of arguments
+ * @argc: the number of arguments
+ * @argv: an array of string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

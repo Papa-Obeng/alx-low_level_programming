b058a12b09075af8d9b59a95130a30d65eba2b94
Papa-Obeng <papaobengn@gmail.com>
2023-01-10 06:18:07 -0800

    Updated 3-Mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,32 +1,88 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - the function for multiplying two numbers
- * @argc - the number of arguments passed through the commandline
- * @argv - a string of array
- * @num1 - the first number
- * @num2 - the second number
- * @result - the result after the multiplication
- * Return: result
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+
+	d = 0;
+
+	n = 0;
+
+	len = 0;
+
+	f = 0;
+
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+
+			if (d % 2)
+				digit = -digit;
+
+			n = n * 10 + digit;
+
+			f = 1;
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	if (argv[1] == '\0' || argv[2] == '\0')
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
 	{
-		printf("error\n");
+		printf("Error\n");
+
 		return (1);
 	}
-	else
-	{
-	printf("%d", mul(argv[1], argv[2]));
-	}
+
+	num1 = _atoi(argv[1]);
+
+	num2 = _atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
 
 
-int mul(int num1, int num2)
-{
-	int result = num1 * num2;
-
-	return (result);
-}

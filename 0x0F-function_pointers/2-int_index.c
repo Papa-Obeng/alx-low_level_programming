80e335aaba1f7a952b5283bb934ba56c2a57f463
Papa-Obeng <papaobengn@gmail.com>
2023-01-27 15:24:59 +0000

    Updated Int Index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,13 +10,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
-		return (-1);
+	if (array && cmp)
+	{
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i] != 0))
 			return (i);
 	}
+	}
+
 	return (-1);
 }
